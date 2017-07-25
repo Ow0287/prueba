48101ec8e122a356eaec1259430ec1ac609aea67
Senasoft <owrivero@misena.edu.co>
2017-07-25 16:12:42 +0200

    dd

--- app/src/main/java/com/example/senasoft/prueba/vista/MainActivity.java
+++ app/src/main/java/com/example/senasoft/prueba/vista/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.senasoft.prueba.vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.senasoft.prueba.R;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
